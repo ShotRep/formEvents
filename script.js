c1aaa54a71f7c8c0d11307df175120b14f8bf89d
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-07-09 13:48:43 -0500

    incremental commit

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const form = document.querySelector('#signup-form')
+const form = document.querySelector("#signup-form")
+const creditCard = document.querySelector("#cc")
+const agreement = document.querySelector("#terms")
+const cards = document.querySelector("#creditCards")
 
-form.addEventListener('submit', function (event) {
-  alert('Your form has been submitted!')
+form.addEventListener("submit", function (event) {
   event.preventDefault() //prevents teh sending on of your data to another page or site.
+  alert("Your form has been submitted!")
+  console.log(event)
+  console.log(creditCard.value)
+  console.log(agreement.checked)
+  console.log(cards.value)
 })
+
+/*
+Takeaways
+1.) We can stop the default behavior of form submission by using preventDefault()
+2.) We have access to the form data
+3.) We can use this data for whatever
+a.) send form data to database
+b.) append form data to page 
+*/
+
+//////////////////////////////////////* INPUT & CHANGE EVENTS */
+
